ead6d3118bf102345018f2a16379a5aa2ee1a2b1
Cfp <cfp@cfp.gotdns.ch>
2022-02-16 15:29:47 +0100

    Logging

--- main.js
+++ main.js
@@ -57,9 +57,11 @@ function sleep(ms) {
         const timeline = edupage.timeline;
         for(let i = 0; i < timeline.length; i++) {
             let msg = timeline[i];
+            console.log("NEW MESSAGE", msg.recipientUserString, msgAllowed.includes(msg.recipientUserString));
             if(msgAllowed.includes(msg.recipientUserString)) {
                 let done = require("./doneid.json");
                 if(!done.msgs.includes(msg.id)) {
+                    console.log("NOT DONE");
                     let channel = bot.channels.cache.get(config.message_channel);
                     if(channel) {
                         console.log("Posting message: " + msg.id);
